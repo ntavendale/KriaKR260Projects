6d216447698219e41e3cd4b0b8f1d9073d507acd
Nigel tavendale <me@nospam.com>
2026-07-21 00:08:51 +0000

    Clean up whitespace

--- KriaAxiDma/dma-proxy-test/TxChannel.pas
+++ KriaAxiDma/dma-proxy-test/TxChannel.pas
@@ -35,15 +35,15 @@ begin
     AChannel^.ChannelBuffers[buffer_id]^.Length := TUtilities.TestSizeBytes;
     if TUtilities.Verify then
     begin
-			for i := 0 to (1-1) do// test_size / sizeof(unsigned int); i++)
-				AChannel^.ChannelBuffers[buffer_id]^.Buffer[i] := i + in_progress_count;
+      for i := 0 to (1-1) do// test_size / sizeof(unsigned int); i++)
+        AChannel^.ChannelBuffers[buffer_id]^.Buffer[i] := i + in_progress_count;
     end;
 
     // Start the DMA transfer and this call is non-blocking
     fpIoctl(AChannel^.FileDescriptor, START_XFER, @buffer_id);
 
-    //Keep track of the number of transfers that are in progress and if the number is less
-		// than the number of channel buffers then stop before all channel buffers are used
+    // Keep track of the number of transfers that are in progress and if the number is less
+    // than the number of channel buffers then stop before all channel buffers are used
     Inc(in_progress_count);
 		if in_progress_count >= TUtilities.TransferCount then
 			BREAK;
@@ -55,49 +55,48 @@ begin
   while (TRUE) do
   begin
     // Perform the DMA transfer and check the status after it completes
-		// as the call blocks til the transfer is done.
-		fpIoctl(AChannel^.FileDescriptor, FINISH_XFER, @buffer_id);
-		if (AChannel^.ChannelBuffers[buffer_id]^.Status <> psNoError) then
-			WriteLn(Format('Proxy tx transfer error %s', [ProxyStatusToString(AChannel^.ChannelBuffers[buffer_id]^.Status)]));
-
-		// Keep track of how many transfers are in progress and how many completed
-		Dec(in_progress_count);
-		Inc(counter);
-
+    // as the call blocks til the transfer is done.
+    fpIoctl(AChannel^.FileDescriptor, FINISH_XFER, @buffer_id);
+    if (AChannel^.ChannelBuffers[buffer_id]^.Status <> psNoError) then
+      WriteLn(Format('Proxy tx transfer error %s', [ProxyStatusToString(AChannel^.ChannelBuffers[buffer_id]^.Status)]));
+    // Keep track of how many transfers are in progress and how many completed
+    Dec(in_progress_count);
+    Inc(counter);
+    
     // If all the transfers are done then exit
     if (counter >= TUtilities.TransferCount) then
-			BREAK;
+      BREAK;
     // If an early stop (control c or kill) has happened then exit gracefully
-		// letting all transfers queued up be completed, but it's trickier because
-		// the number of transmit vs receive channel buffers can be very different
-		// which means another X transfers need to be done gracefully shutdown the
-		// receive without leaving transfers in progress which is unrecoverable
+    // letting all transfers queued up be completed, but it's trickier because
+    // the number of transmit vs receive channel buffers can be very different
+    // which means another X transfers need to be done gracefully shutdown the
+    // receive without leaving transfers in progress which is unrecoverable
 		if (TUtilities.Stop and not stop_in_progress)  then
     begin
-			stop_in_progress := TRUE;
-    	TUtilities.TransferCount := counter + RX_BUFFER_COUNT;
+      stop_in_progress := TRUE;
+      TUtilities.TransferCount := counter + RX_BUFFER_COUNT;
     end;
-
-    // If the ones in progress will complete the count then don't start more */
-		if ((counter + in_progress_count) >= TUtilities.TransferCount) then
+    
+    // If the ones in progress will complete the count then don't start more
+    if ((counter + in_progress_count) >= TUtilities.TransferCount) then
     begin
-			buffer_id := buffer_id + BUFFER_INCREMENT;
-		  buffer_id := buffer_id mod TX_BUFFER_COUNT;
+      buffer_id := buffer_id + BUFFER_INCREMENT;
+      buffer_id := buffer_id mod TX_BUFFER_COUNT;
       CONTINUE;
     end;
 
     // Initialize the buffer and perform the DMA transfer, check the status after it completes
-		// as the call blocks til the transfer is done.
-		if (TUtilities.Verify) then
+    // as the call blocks til the transfer is done.
+    if (TUtilities.Verify) then
     begin
-			for i := 0 to (TUtilities.TestSizeBytes div sizeof(Cardinal)) -1 do
-				AChannel^.ChannelBuffers[buffer_id]^.Buffer[i] := i + ((TX_BUFFER_COUNT div BUFFER_INCREMENT) - 1) + counter;
-		end;
-
-		// Restart the completed channel buffer to start another transfer and keep
-		// track of the number of transfers in progress
-		fpIoctl(AChannel^.FileDescriptor, START_XFER, @buffer_id);
-		Inc(in_progress_count);
+      for i := 0 to (TUtilities.TestSizeBytes div sizeof(Cardinal)) -1 do
+        AChannel^.ChannelBuffers[buffer_id]^.Buffer[i] := i + ((TX_BUFFER_COUNT div BUFFER_INCREMENT) - 1) + counter;
+    end;
+    
+    // Restart the completed channel buffer to start another transfer and keep
+    // track of the number of transfers in progress
+    fpIoctl(AChannel^.FileDescriptor, START_XFER, @buffer_id);
+    Inc(in_progress_count);
   end;
   Result := nil;
 end;
