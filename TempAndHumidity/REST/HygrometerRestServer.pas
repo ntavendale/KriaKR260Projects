ee289d3a5c2e009f917c1c441681b366afaba78f
Nigel tavendale <me@nospam.com>
2026-09-08 01:45:43 +0000

    Post Resolution Select Data

--- TempAndHumidity/REST/HygrometerRestServer.pas
+++ TempAndHumidity/REST/HygrometerRestServer.pas
@@ -7,19 +7,18 @@ uses
   mormot.core.base,  mormot.core.data,  mormot.core.os,  mormot.core.log,  mormot.core.text,  mormot.core.json,
   mormot.core.search,  mormot.core.buffers,  mormot.core.unicode,  mormot.crypt.secure,  mormot.orm.base,  mormot.orm.core,
   mormot.rest.core,  mormot.rest.server,  mormot.rest.http.server,  mormot.rest.memserver, 
-  Instruction, HygrometerData, RxChannel;
+  Instruction, HygrometerData, Resolution, RxChannel, TxChannel;
 
 type
   THygrometerRestServer = class(TRestServerFullMemory)
   published
     procedure get_data(pmCtxt: TRestServerUriContext);
+    procedure set_resolution(pmCtxt: TRestServerUriContext);
     //procedure SetDisplay(pmCtxt: TRestServerUriContext);
     //procedure SetResolution(pmCtxt: TRestServerUriContext);
   end;
 
 implementation
-
-
 
 procedure THygrometerRestServer.get_data(pmCtxt: TRestServerUriContext);
 var
@@ -34,8 +33,7 @@ begin
 
   LInstruction := TInstruction.Create(itFetch, 0);
   try
-    WriteLn('Instruction Type: ', LInstruction.InstructionType);
-    WriteLn('Instruction Data: ', LInstruction.InstructionData);
+    SendData(LInstruction.GetInstruction);
   finally
     LInstruction.Free;
   end;
@@ -46,6 +44,39 @@ begin
     pmCtxt.Returns(LHygrometerData.ToJson, HTTP_SUCCESS);
   finally
     LHygrometerData.Free;    
+  end;
+end;
+
+procedure THygrometerRestServer.set_resolution(pmCtxt: TRestServerUriContext);
+var
+  LInstruction: TInstruction;
+  LResolution: TResolution;
+  json_data_in: String;
+begin
+  if (pmCtxt.Method <> mPOST) then
+  begin
+     pmCtxt.Error(StringToUtf8('Only http GET allowed'), HTTP_BADREQUEST);
+     EXIT;
+  end;
+
+  json_data_in := pmCtxt.Call.InBody;
+  LResolution := TResolution.FromJson(json_data_in);
+  if (nil = LResolution) then
+  begin
+    pmCtxt.Error(StringToUtf8('Invalid Json'), HTTP_BADREQUEST);
+    EXIT;
+  end;
+
+  try
+    LInstruction := TInstruction.Create(itSetResolution, LResolution.GetResolution);
+    try
+      SendData(LInstruction.GetInstruction)
+    finally
+      LInstruction.Free;
+    end;
+    pmCtxt.Success;
+  finally
+    LResolution.Free;
   end;
 end;
 
---
+++ TempAndHumidity/REST/Resolution.pas
@@ -0,0 +1,71 @@
+{$MODE DELPHIUNICODE}
+unit Resolution;
+
+interface
+
+uses
+  SysUtils,
+  mormot.core.base,
+  mormot.core.text,
+  mormot.core.json;
+
+type
+  TTemperatureResolution = (tr14Bit, tr11Bit);
+  THumidityResolution = (hr14Bit, hr11Bit, hr8Bit);
+  TResolution = class(TSynPersistent)
+  private
+    FTemperatureResolution: TTemperatureResolution;
+    FHumidityResolution: THumidityResolution;
+  published
+    property TemperatureResolution: TTemperatureResolution read FTemperatureResolution write FTemperatureResolution;
+    property HumidityResolution: THumidityResolution read FHumidityResolution write FHumidityResolution;
+  public
+    constructor Create(ATemperatureResolution: TTemperatureResolution; AHumidityResolution: THumidityResolution); reintroduce;
+    function GetResolution: Cardinal;
+    function ToJson: String;
+    class function FromJson(const AJson: String): TResolution;
+  end;
+
+implementation
+
+constructor TResolution.Create(ATemperatureResolution: TTemperatureResolution; AHumidityResolution: THumidityResolution);
+begin
+  inherited Create;
+  FTemperatureResolution := ATemperatureResolution;
+  FHumidityResolution := AHumidityResolution;
+end;
+
+function TResolution.GetResolution: Cardinal;
+var
+  LResolutionData: Cardinal;
+begin
+  case FTemperatureResolution of
+    tr11Bit: LResolutionData := 11;
+  else
+    LResolutionData := 14;
+  end;
+  case FHumidityResolution of
+    hr8Bit: LResolutionData := (LResolutionData shl 8) or $00000008;
+    hr11Bit: LResolutionData := (LResolutionData shl 8) or $0000000B;
+  else
+    LResolutionData := (LResolutionData shl 8) or $0000000E;
+  end;
+  Result := LResolutionData;
+end;
+
+function TResolution.ToJson: String;
+begin
+  Result := ObjectToJson(self);
+end;
+
+class function TResolution.FromJson(const AJson: String): TResolution;
+begin
+  Result := TResolution.Create(tr14Bit, hr14Bit);
+  if not ObjectLoadJson(result, RawUtf8(AJson)) then
+  begin
+    Result.Free;
+    Result := nil;
+  end;
+end;
+
+end.
