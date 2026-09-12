396c35be53ed3d0060a41c7d819d8709c304bc93
Nigel tavendale <me@nospam.com>
2026-09-12 18:01:48 +0000

    Add Copyright notice

--- TempAndHumidity/REST/HygrometerData.pas
+++ TempAndHumidity/REST/HygrometerData.pas
@@ -1,3 +1,18 @@
+// Copyright 2026 Nigel Tavendale
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this code
+// associated documentation files (the "Code"), to deal in the Code without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Code, and to permit persons to whom the Code is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial
+// portions of the Code.
+//
+// THE CODE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+// SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE CODE OR THE USE OR
+// OTHER DEALINGS IN THE CODE.
 {$MODE DELPHIUNICODE}
 unit HygrometerData;
 
@@ -27,9 +42,9 @@ constructor THygrometerData.Create(ATemperatureHumidityData: Cardinal);
 begin
   inherited Create;
   // Temperature is lower word
-  FTemperature := (((ATemperatureHumidityData and $0000FFFF) / 65536.0) * 165.0) - 40.0;
+  FTemperature := (((ATemperatureHumidityData  shr 16) / 65536.0) * 165.0) - 40.0;
   // Humidity is upper word
-  FHumidity := ((ATemperatureHumidityData shr 16) / 65536.0) * 100.0
+  FHumidity := ((ATemperatureHumidityData and $0000FFFF) / 65536.0) * 100.0
 end;
 
 constructor THygrometerData.Create(ATemperatureData: Word; AHumidityData: Word);
